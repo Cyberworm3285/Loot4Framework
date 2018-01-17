d526e813f00862b782c79e49c5c9bf0b348ce88e
cyberworm3285 <cyberworm3285@web.de>
2018-01-17 22:44:49 +0100

    ...

--- Loot4Standard/Loot4Standard/Extensions/LINQ_Extensions.cs
+++ Loot4Standard/Loot4Standard/Extensions/LINQ_Extensions.cs
@@ -6,11 +6,12 @@ namespace Loot4Standard.Extensions
 {
     public static class LINQ_Extensions
     {
-        public static void ForEach<T>(this IEnumerable<T> en, Action<T> action)
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> en, Action<T> action)
         {
             foreach (var x in en)
             {
                 action(x);
+                yield return x;
             }
         }
     }
